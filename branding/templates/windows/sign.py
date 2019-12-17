dc57e1769db9a68c1f8258a7384fd411a8c9d18c
Kali Kaneko (leap communications) <kali@leap.se>
2019-12-17 17:17:40 +0100

    [pkg] modify windows git path in signing script

--- branding/templates/windows/sign.py
+++ branding/templates/windows/sign.py
@@ -2,8 +2,6 @@
 """
 This script is expected to be called from the main makefile, that should pass
 the content of the WIN_CERT_PASS variable as the second argument.
-
-Just make sure that $GOPATH is properly configured.
 """
 import subprocess
 import os
@@ -13,10 +11,8 @@ WIN_CERT_PATH = sys.argv[1]
 WIN_CERT_PASS = sys.argv[2]
 SIGNTOOL = "signtool"
 
-GOPATH = os.environ.get('GOPATH')
 VERSION = subprocess.run(
-    'git -C ' + GOPATH +
-    '\\src\\0xacab.org\\leap\\bitmask-vpn describe --tags',
+    'git describe --tags',
     stdout=subprocess.PIPE).stdout.strip()
 
 installer = "RiseupVPN-" + str(VERSION, 'utf-8') + '.exe'
