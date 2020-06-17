9b8009cfaf6707d23a20494cd5467aed9c98513b
kali kaneko (leap communications) <kali@leap.se>
2020-06-17 19:40:24 +0200

    [feat] cleanup temp dirs on quit

---
+++ pkg/backend/actions.go
@@ -0,0 +1,21 @@
+package backend
+
+import (
+	"log"
+	"os"
+)
+
+func startVPN() {
+	err := ctx.bm.StartVPN(ctx.Provider)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func stopVPN() {
+	err := ctx.bm.StopVPN()
+	if err != nil {
+		log.Println(err)
+	}
+}
