006bfb04789f94b458119fee7c0544bf9b04bfce
kali kaneko (leap communications) <kali@leap.se>
2020-09-07 17:21:28 +0200

    [config] test with riseup config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,15 +7,4 @@ var (
 	Auth            = ""
 	APIURL          = ""
 	GeolocationAPI  = ""
-)
-
-var Version string
-
-/*
-
-CaCert : a string containing a representation of the provider CA, used to
-        sign the webapp and openvpn certificates. should be placed in
-        config/[provider]-ca.crt
-
-*/
 var CaCert = []byte("")
