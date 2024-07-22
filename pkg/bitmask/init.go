leapcode/bitmask-vpn

commit 970c3f4fb0c854baef548c1bc32742c9b31affb6

Add support for different api versions
- vendor.conf now has a default api version:
    - apiVersion = 3 is set for all current providers
- provider.py (called during `make vendor`) now supports api version
  and writes it to providers.json
- the desktop client logs the used api version. Currently v5 is not yet implement.
  This is just the preparation for it
- this is only a temporary solution. In the future, we want to get the
  api version during runtime by parsing $api/provider.json

pkg/bitmask/init.go
@@ -16,12 +16,13 @@
 package bitmask
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
 
-	"0xacab.org/leap/bitmask-vpn/pkg/config"
 	"0xacab.org/leap/bitmask-vpn/pkg/vpn"
+	"0xacab.org/leap/bitmask-vpn/pkg/config"
 )
 
 type ProviderInfo struct {
@@ -43,6 +44,7 @@ type ProviderOpts struct {
 	GeolocationURL  string `json:"geolocationAPI"`
 	AskForDonations bool   `json:"askForDonations"`
 	CaCert          string `json:"caCertString"`
+	ApiVersion      int    `json:"apiVersion"`
 }
 
 func GetConfiguredProvider() *ProviderInfo {
@@ -59,6 +61,7 @@ func ConfigureProvider(opts *ProviderOpts) {
 	config.GeolocationAPI = opts.GeolocationURL
 	config.APIURL = opts.ApiURL
 	config.CaCert = []byte(opts.CaCert)
+	config.ApiVersion = opts.ApiVersion
 }
 
 func InitializeLogger() {
@@ -66,12 +69,17 @@ func InitializeLogger() {
 	if err != nil {
 		log.Println("Can't configure logger: ", err)
 	}
+
 }
 
-func initBitmaskVPN() (Bitmask, error) {
-	b, err := vpn.Init()
-	if err != nil {
-		log.Printf("An error ocurred starting bitmask vpn: %v", err)
+func initBitmaskVPN() (b Bitmask, err error) {
+	if config.ApiVersion == 5 {
+		return nil, fmt.Errorf("API v5 is not implemented. Please use apiVersion=3 in config file")
+	} else {
+		b, err = vpn.Init()
+		if err != nil {
+			log.Printf("An error ocurred starting bitmask vpn: %v", err)
+		}
 	}
 	return b, err
 }

branding/scripts/init
@@ -114,6 +114,7 @@ binaryName          = {provider}-vpn
 providerURL         = https://{providerURL}
 auth                = anon
 apiURL              = https://api.{providerURL}/
+apiVersion          = 3
 caURL               = https://{providerURL}/ca.crt
 
 infoURL             = https://{providerURL}/vpn

branding/scripts/provider.py
@@ -19,18 +19,23 @@ def getProviderData(provider, config):
         c = config[provider]
     except Exception:
         raise ValueError('Cannot find provider')
-        
+
     d = dict()
     keys = ('name', 'applicationName', 'binaryName', 'auth', 'authEmptyPass',
             'providerURL', 'tosURL', 'helpURL',
             'askForDonations', 'donateURL', 'apiURL',
-            'geolocationAPI', 'caCertString')
+            'apiVersion', 'geolocationAPI', 'caCertString')
     boolValues = ['askForDonations', 'authEmptyPass']
+    intValues = ['apiVersion', ]
 
     for value in keys:
+        if value not in c:
+            continue
         d[value] = c.get(value)
         if value in boolValues:
-            d[value] = c.getboolean(value)
+            d[value] = bool(d[value])
+        elif value in intValues:
+            d[value] = int(d[value])
 
     d['timeStamp'] = '{:%Y-%m-%d %H:%M:%S}'.format(
         datetime.datetime.now())

pkg/config/config.go
@@ -7,5 +7,6 @@ var (
 	Auth            = ""
 	APIURL          = ""
 	GeolocationAPI  = ""
+	ApiVersion      = -1
 )
 var CaCert = []byte("")

providers/vendor.conf
@@ -11,6 +11,7 @@ binaryName          = riseup-vpn
 providerURL         = riseup.net
 auth                = anon
 apiURL              = https://api.black.riseup.net/
+apiVersion          = 3
 caURL               = https://black.riseup.net/ca.crt
 
 infoURL             = https://riseup.net/vpn
@@ -30,6 +31,7 @@ auth                = anon
 
 providerURL         = float.hexacab.org
 apiURL              = https://api.float.hexacab.org:4430/
+apiVersion          = 3
 caURL               = https://api.float.hexacab.org/ca.crt
 
 infoURL             = https://float.hexacab.org/
@@ -70,6 +72,7 @@ authEmptyPass       = true
 
 providerURL         = vpnlib.bitmask.net
 apiURL              = https://api.vpnlib.bitmask.net:4430/
+apiVersion          = 3
 caURL               = https://api.vpnlib.bitmask.net/ca.crt
 
 infoURL             = https://libraryvpn.org/
@@ -89,6 +92,7 @@ auth                = anon
 
 providerURL         = float.bitmask.net
 apiURL              = https://api.float.bitmask.net:4430/
+apiVersion          = 3
 caURL               = https://api.float.bitmask.net/ca.crt
 
 infoURL             = https://float.bitmask.net/
@@ -109,6 +113,7 @@ binaryName          = bitmask-vpn
 providerURL         = https://demo.bitmask.net
 auth                = anon
 apiURL              = https://api.demo.bitmask.net/
+apiVersion          = 3
 caURL               = https://api.demo.bitmask.net/ca.crt
 
 infoURL             = https://demo.bitmask.net/vpn

commit fe2551381f63edb5aca357e8c9565484372a4b04

Do not fail during initialization if apiVersion is 5
This was introduced in 970c3f4fb where the code supported different api
versions, but there was not yet an implementation for v5.
The apiVersion can be set via env. In the future apiVersion will be
fetched from the API using the /providers.json URL"

pkg/bitmask/init.go
@@ -16,7 +16,6 @@
 package bitmask
 
 import (
-	"fmt"
 	"os"
 
 	"0xacab.org/leap/bitmask-vpn/pkg/config"
@@ -64,9 +63,6 @@ func ConfigureProvider(opts *ProviderOpts) {
 }
 
 func initBitmaskVPN() (Bitmask, error) {
-	if config.ApiVersion == 5 {
-		return nil, fmt.Errorf("API v5 is not implemented. Please use apiVersion=3 in config file")
-	}
 	b, err := vpn.Init()
 	return b, err
 }